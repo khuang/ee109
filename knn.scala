2ea0b2292623d2ce6fd30d9677622219f6c0aeeb
Zach Irving Belateche <zachbela@tucson.Stanford.EDU>
2020-05-17 20:47:46 -0700

    created a distlabel struct

--- knn.scala
+++ knn.scala
@@ -3,6 +3,11 @@ import spatial.dsl._
 import scala.math.abs
 
 
+@struct class DistLabel(
+  dist: Float,
+  label: Int
+)
+
 @spatial object KNN extends SpatialApp {
   def main(args: Array[String]): Unit = {
 
@@ -14,24 +19,29 @@ import scala.math.abs
     //dummy data
     val test_set = (0::test_size, 0::v_len){(i,j) => i.to[Float]}
     val train_set = (0::train_size, 0::v_len){(i,j) => (10 - i).to[Float]}
-    val train_labels = scala.Array(1,2)
+    val train_labels_scala = scala.Array(1,2,1,2,1,2,1,2,1,2)
+    val train_labels = toSpatialIntArray(train_labels_scala)
 
     print("test")
     printMatrix(test_set)
     print("train")
     printMatrix(train_set)
+    print("labels")
+    printArray(train_labels)
 
     val dTrain = DRAM[Float](train_size.to[Int], v_len.to[Int])
     val dTest = DRAM[Float](test_size.to[Int], v_len.to[Int])
+    val dLabels = DRAM[Int](train_size.to[Int])
 
     val classification = ArgOut[Int]
 
     setMem(dTrain, train_set)
     setMem(dTest, test_set)
+    setMem(dLabels, train_labels)
 
     // temp test vars
-    val outputDRAM = DRAM[Float](train_size.to[Int], test_size.to[Int])
-    val sortedDRAM = DRAM[Float](k.to[Int], test_size.to[Int])
+    val outputDRAM = DRAM[DistLabel](train_size.to[Int], test_size.to[Int])
+    val sortedDRAM = DRAM[DistLabel](k.to[Int], test_size.to[Int])
 
     Accel {
       val nTrain = 10.to[Int]
@@ -48,11 +58,13 @@ import scala.math.abs
 
       val test_sram = SRAM[Float](nTest, vLen)
       val train_sram = SRAM[Float](nTrain, vLen)
-      val distances = SRAM[Float](nTrain, nTest)
+      val label_sram = SRAM[Int](nTrain)
+      val distances = SRAM[DistLabel](nTrain, nTest)
 
       test_sram load dTest(base :: nTest par load_par, base :: vLen)
       train_sram load dTrain(base :: nTrain par load_par, base :: vLen)
-
+      label_sram load dLabels(base :: nTrain par load_par)
+      
       Foreach(nTest by step par test_par){ test_idx =>
         Foreach(nTrain by step par train_par){ train_idx =>
           val distance = Reg[Float](0)
@@ -60,38 +72,35 @@ import scala.math.abs
             val pos = test_sram(test_idx, i) - train_sram(train_idx, i)
             val neg = train_sram(train_idx, i) - test_sram(test_idx, i)
             val abs = mux(pos > neg, pos, neg) //jank absolute value function
-            //println("abs")
-            //println(abs)
             abs
           }{_+_} //this is the manhattan distance
-          //println("dist")
-          //println(distance)
-          distances(train_idx, test_idx) = distance
+          distances(train_idx, test_idx) = DistLabel(distance, label_sram(train_idx))
         }
       }
 
       outputDRAM(base :: nTrain, base :: nTest) store distances
 
-      val sorted_sram = SRAM[Float](k, nTest)
-      val sorted_labels_sram = SRAM[Float](k, nTest)
+      val sorted_sram = SRAM[DistLabel](k, nTest)
 
       val sort_par = 1.to[Int]
       val max_dist = 100.to[Float]
 
       val old_dist = RegFile[Float](nTest)
+      val filler_distlabel = DistLabel(max_dist, 0)
 
       Foreach(nTest par test_par){ test_idx => 
-        //old_dist(test_idx) = 0.to[Int]
         Sequential.Foreach(k by 1){ k_id =>
-          val best = Reg[Float](0)
+          val best = Reg[DistLabel]
         
           Reduce(best)(nTrain by 1 par sort_par){ train_idx =>
-            val dist = distances(train_idx, test_idx)
-            mux(dist <= old_dist(test_idx), max_dist, dist)
-          }{(a,b) => mux(a<b, a, b)}
+            val elem = distances(train_idx, test_idx)
+            val dist = elem.dist
+            val label = elem.label
+            mux(dist <= old_dist(test_idx), filler_distlabel, elem)
+          }{(a,b) => mux(a.dist<b.dist, a, b)}
           
           sorted_sram(k_id, test_idx) = best
-          old_dist(test_idx) = best
+          old_dist(test_idx) = best.dist
         }
       }
 
