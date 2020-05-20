ac494c24dc027fc6479b81371888a70efacf6060
zbelateche <zachbela@stanford.edu>
2020-05-19 17:05:48 -0700

    seperated csvs

--- make_csv.py
+++ make_csv.py
@@ -12,19 +12,22 @@ y = iris.target
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.1, random_state = 4)
 
-with open('iris.csv', mode='w') as csv_file:
+with open('train.csv', mode='w') as csv_file:
     writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-
-    # writer.writerow([len(x_train)])
     for xtr in x_train:
         writer.writerow(xtr)
 
+with open('train_labels.csv', mode='w') as csv_file:
+    writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     for ytr in y_train:
         writer.writerow([ytr])
 
-    # writer.writerow([len(x_test)])
+with open('test.csv', mode='w') as csv_file:
+    writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     for xtr in x_test:
         writer.writerow(xtr)
 
+with open('test_labels.csv', mode='w') as csv_file:
+    writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     for ytr in y_test:
         writer.writerow([ytr])
